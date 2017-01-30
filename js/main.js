e76da436442d8eb35864a4ccee8d8096e9b3c90f
Guest User <Guest@Ians-MacBook-Air.local>
2017-01-30 11:11:06 -0800

    Added fonts and Win Count

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+console.log("connected");
+
 var tableID;
 var turnCount=0;
 var x, y;
@@ -7,6 +9,7 @@ var logicArray = [
 	[3, 11, 10]
 ];
 var winnerFound = false;
+var xWins=0, oWins=0;
 
 
 //Add Click Logic
@@ -33,12 +36,13 @@ for (var i=1; i<4; i++){
 	}
 }
 
+// Reset Button
 document.getElementById('reset').onclick = function(){
 	for(var i=0; i<=9; i++){
 		document.getElementsByTagName('td')[i].innerHTML="";
 		winnerFound = false;
 		turnCount = 0;
-		document.getElementsByTagName('h1')[0].innerHTML="";	
+		document.getElementsByTagName('h2')[0].innerHTML="";	
 		logicArray = [
 			[4, 5, 6],
 			[9, 8, 7],
@@ -48,97 +52,52 @@ document.getElementById('reset').onclick = function(){
 	
 }
 
+// Function to check the winner
 function checkWinner(){
 	if (logicArray[0][0]===logicArray[0][1] && logicArray[0][0]===logicArray[0][2]){
 		winnerFound = true;
-		document.getElementsByTagName('h1')[0].innerHTML = document.getElementById('1_1').innerHTML+" is the winner!";
+		document.getElementsByTagName('h2')[0].innerHTML = document.getElementById('1_1').innerHTML+" is the winner!";
+		winCount(xWins, oWins, document.getElementById('1_1').innerHTML);
 	} else if (logicArray[1][0]===logicArray[1][1] && logicArray[1][0]===logicArray[1][2]) {
 		winnerFound = true;
-		document.getElementsByTagName('h1')[0].innerHTML = document.getElementById('2_1').innerHTML+" is the winner!";
+		document.getElementsByTagName('h2')[0].innerHTML = document.getElementById('2_1').innerHTML+" is the winner!";
+		winCount(xWins, oWins, document.getElementById('2_1').innerHTML);
 	} else if (logicArray[2][0]===logicArray[2][1] && logicArray[2][0]===logicArray[2][2]) {
 		winnerFound = true;
-		document.getElementsByTagName('h1')[0].innerHTML = document.getElementById('3_1').innerHTML+" is the winner!";
+		document.getElementsByTagName('h2')[0].innerHTML = document.getElementById('3_1').innerHTML+" is the winner!";
+		winCount(xWins, oWins, document.getElementById('3_1').innerHTML);
 	} else if (logicArray[0][0]===logicArray[1][0] && logicArray[1][0]===logicArray[2][0]) {
 		winnerFound = true;
-		document.getElementsByTagName('h1')[0].innerHTML = document.getElementById('1_1').innerHTML+" is the winner!";
+		document.getElementsByTagName('h2')[0].innerHTML = document.getElementById('1_1').innerHTML+" is the winner!";
+		winCount(xWins, oWins, document.getElementById('1_1').innerHTML);
 	} else if (logicArray[0][1]===logicArray[1][1] && logicArray[1][1]===logicArray[2][1]) {
 		winnerFound = true;
-		document.getElementsByTagName('h1')[0].innerHTML = document.getElementById('1_2').innerHTML+" is the winner!";
+		document.getElementsByTagName('h2')[0].innerHTML = document.getElementById('1_2').innerHTML+" is the winner!";
+		winCount(xWins, oWins, document.getElementById('1_2').innerHTML);
 	} else if (logicArray[0][2]===logicArray[1][2] && logicArray[1][2]===logicArray[2][2]) {
 		winnerFound = true;
-		document.getElementsByTagName('h1')[0].innerHTML = document.getElementById('1_3').innerHTML+" is the winner!";
+		document.getElementsByTagName('h2')[0].innerHTML = document.getElementById('1_3').innerHTML+" is the winner!";
+		winCount(xWins, oWins, document.getElementById('1_3').innerHTML);
 	} else if (logicArray[0][0]===logicArray[1][1] && logicArray[1][1]===logicArray[2][2]) {
 		winnerFound = true;
-		document.getElementsByTagName('h1')[0].innerHTML = document.getElementById('1_1').innerHTML+" is the winner!";
+		document.getElementsByTagName('h2')[0].innerHTML = document.getElementById('1_1').innerHTML+" is the winner!";
+		winCount(xWins, oWins, document.getElementById('1_1').innerHTML);
 	} else if (logicArray[0][2]===logicArray[1][1] && logicArray[1][1]===logicArray[2][0]) {
 		winnerFound = true;
-		document.getElementsByTagName('h1')[0].innerHTML = document.getElementById('1_3').innerHTML+" is the winner!";
+		document.getElementsByTagName('h2')[0].innerHTML = document.getElementById('1_3').innerHTML+" is the winner!";
+		winCount(xWins, oWins, document.getElementById('1_3').innerHTML);
 	} else if (turnCount === 9){
-		document.getElementsByTagName('h1')[0].innerHTML = "Cats game!"
+		document.getElementsByTagName('h2')[0].innerHTML = "Cats game!"
 	}
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//lots of white space
+function winCount(xWins, oWins, winner) {
+	console.log(winner);
+	if (winner === 'X') {
+		xWins++;
+		document.getElementById('xWins').innerHTML = "X: "+xWins+" wins";
+	} else {
+		oWins++;
+		document.getElementById('oWins').innerHTML = "O: "+oWins+" wins";
+	}
+}
